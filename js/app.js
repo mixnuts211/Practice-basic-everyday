a72b13c4b46e54629e1820595ff3b5acf55b0afe
MiGyeong Kim <migyeong0211@gmail.com>
2023-09-05 18:06:18 +0900

    Feat : deploy page layout

---
+++ js/app.js
@@ -0,0 +1,26 @@
+const getWorkListData = async () => {
+  const res = await fetch("../listData.json");
+  const getListData = await res.json();
+  return getListData;
+};
+
+const paintWorkDataList = async () => {
+  const workDataList = await getWorkListData();
+  console.log(workDataList);
+  document.querySelector(".work-list").innerHTML = workDataList.newbie
+    .map(
+      (workData) => `
+    <li>
+        <a href="${workData.link}">
+            <div class="work-list-img-wrap">
+                <img src="${workData.img}" alt="${workData.title}" />
+            </div>
+            <span class="work-list-txt">${workData.title}</span>
+        </a>
+    </li>
+  `
+    )
+    .join("");
+};
+
+paintWorkDataList();
